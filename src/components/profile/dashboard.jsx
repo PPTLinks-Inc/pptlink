a61587c2576ca471408dbb57b547670cd25200cd
Omizegba <flamanyoman@gmail.com>
2023-08-30 11:51:31 +0100

    Created all the accessory pages

---
+++ src/components/profile/dashboard.jsx
@@ -0,0 +1,7 @@
+const Dashboard =() =>{
+
+    return (
+        <div>this is a dashboard</div>
+    )
+}
+export default Dashboard
---
+++ src/components/upload/upload.jsx
@@ -0,0 +1,7 @@
+const Upload = () =>{
+
+    return (
+        <div>An Upload page</div>
+    )
+}
+export default Upload;
