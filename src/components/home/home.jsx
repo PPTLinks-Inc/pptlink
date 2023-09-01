3f20f256ca3bbc0dd31a3296f123614de96310ae
Omizegba <flamanyoman@gmail.com>
2023-09-01 16:04:29 +0100

    Pushing the home page ui

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,25 @@
-const Home = () => {
-    
-    return (
-        <div>Home</div>
-    )
-}
+// import MenuIcon from '@mui/icons-material/Menu';
 
-export default Home
+const Home = () => {
+  return (
+    <div className='box-border w-full min-h-max bg-slate-200 relative'>
+      <div className=' min-h-screen bg-black min-w-full absolute top-[5px] rounded-t-[2.7rem] overflow-hidden text-slate-200 px-[4rem]'>
+        <div className='h-24 flex justify-between items-center'>
+          <div>
+            <p>PPTLink</p>
+          </div>
+
+          <div>
+            <button className='px-7 rounded-3xl py-1 bg-slate-200 text-black'>
+              Present
+            </button>
+
+            {/* <MenuIcon className='rounded-full w-7 h-7 bg-slate-200 border-green-900' /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
