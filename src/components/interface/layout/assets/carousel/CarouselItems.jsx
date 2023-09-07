58708e61b88dee9fc59f60ada7e152bb406fb9ba
beekay19 <bknelson19@gmail.com>
2023-09-07 14:03:27 +0100

    fixed image ratio

--- src/components/interface/layout/assets/carousel/CarouselItems.jsx
+++ src/components/interface/layout/assets/carousel/CarouselItems.jsx
@@ -2,7 +2,7 @@ function CarouselItems({item,active}) {
 
   return (
 <li className="carousel__slides shrink-0 w-full transition-all duration-300" style={{transform: `translateX(${-active * 100}%) `}}>
-    <img src={item} alt="" className="w-full h-full object-cover" width={'100%'} />
+    <img src={item} alt="" className="w-full h-full object-contain" width={'100%'} />
 </li>
   )
 }
