8367d500a4570311c7299e8a32cda8b286e85749
dextemmy <abimbolamujeeb789@gmail.com>
2023-09-17 21:07:54 +0100

    responsive correctly

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <section className='min-h-full w-[100%] flex px-7 md:px-[25%] '>
+    <section className='min-h-full w-[100%] flex px-7 lg:px-[25%] '>
       <div className='h-[80vh] w-full flex flex-col justify-between'>
         <h1 className='text-[40px] my-6 font-medium'>
           404: We could not find this page
@@ -15,27 +15,27 @@ const NotFound = () => {
 
         <Link
           to=''
-          className='border-l-4 pl-2 my-2 border-slate-200 h-[5rem] md:h-[50px] flex items-center'
+          className='border-l-4 pl-2 my-2 border-slate-200 h-[5rem] lg:h-[50px] flex items-center'
         >
           Home - takes you back to the home page
         </Link>
         <Link
           to=''
-          className='border-l-4 pl-2 my-2 border-slate-200 h-[5rem] md:h-[50px] flex items-center'
+          className='border-l-4 pl-2 my-2 border-slate-200 h-[5rem] lg:h-[50px] flex items-center'
         >
           Present - If you need to make a presentation, click and follow the
           intructions
         </Link>
         <Link
           to=''
-          className='border-l-4 pl-2 my-2 border-slate-200 h-[5rem] md:h-[50px] flex items-center'
+          className='border-l-4 pl-2 my-2 border-slate-200 h-[5rem] lg:h-[50px] flex items-center'
         >
           Log in - Already registered? then log in, else sign up and join the
           platform
         </Link>
         <Link
           to=''
-          className='border-l-4 pl-2 my-2 border-slate-200 h-[5rem] md:h-[50px] flex items-center'
+          className='border-l-4 pl-2 my-2 border-slate-200 h-[5rem] lg:h-[50px] flex items-center'
         >
           Institutions - If you are looking for presentations belonging to an
           institution
--- src/components/root/root.jsx
+++ src/components/root/root.jsx
@@ -23,7 +23,7 @@ export default function Root() {
 
     return (
         <>
-            <div className='w-full min-h-[100%] bg-slate-200 relative flex-wrap flex-col'>
+            <div className='w-full h-[100%] bg-slate-200 relative flex-wrap flex-col'>
                 <div>
                     {page.dropdown && (
                         <div className='h-[8rem] flex justify-between items-center py-3 px-3 border lg:p-[2.5rem] lg:h-[10rem]'>
@@ -52,19 +52,19 @@ export default function Root() {
                 </div>
 
                 <div className='flex-col w-[100%] flex-1 border justify-center flex lg:flex-wrap lg:flex-row '>
-                    <Link className='border text-center border-collapse border-slate-100 w-[90%] font-medium py-5 mx-auto text-[40px] hover:bg-slate-100 lg:w-1/2 lg:flex lg:items-center lg:mx-0 lg:px-14  lg:text-[40px] lg:py-14'>
+                    <Link className='border text-center border-collapse border-slate-100 w-[90%] font-medium py-3 mx-auto text-[40px] hover:bg-slate-100 lg:w-1/2 lg:flex lg:items-center lg:mx-0 lg:px-14  lg:text-[40px] lg:py-14'>
                         Home
                     </Link>
 
-                    <Link className='border text-center border-collapse border-slate-100 w-[90%] font-medium py-5 mx-auto text-[40px] hover:bg-slate-100 lg:w-1/2 lg:flex lg:items-center lg:mx-0 lg:px-14  lg:text-[40px] lg:py-14'>
+                    <Link className='border text-center border-collapse border-slate-100 w-[90%] font-medium py-3 mx-auto text-[40px] hover:bg-slate-100 lg:w-1/2 lg:flex lg:items-center lg:mx-0 lg:px-14  lg:text-[40px] lg:py-14'>
                         Institutions
                     </Link>
 
-                    <Link className='border text-center border-collapse border-slate-100 w-[90%] font-medium py-5 mx-auto text-[40px] hover:bg-slate-100 lg:w-1/2 lg:flex lg:items-center lg:mx-0 lg:px-14  lg:text-[40px] lg:py-14'>
+                    <Link className='border text-center border-collapse border-slate-100 w-[90%] font-medium py-3 mx-auto text-[40px] hover:bg-slate-100 lg:w-1/2 lg:flex lg:items-center lg:mx-0 lg:px-14  lg:text-[40px] lg:py-14'>
                         About
                     </Link>
 
-                    <Link className='border text-center border-collapse border-slate-100 w-[90%] font-medium py-5 mx-auto text-[40px] hover:bg-slate-100 lg:w-1/2 lg:flex lg:items-center lg:mx-0 lg:px-14  lg:text-[40px] lg:py-14 '>
+                    <Link className='border text-center border-collapse border-slate-100 w-[90%] font-medium py-3 mx-auto text-[40px] hover:bg-slate-100 lg:w-1/2 lg:flex lg:items-center lg:mx-0 lg:px-14  lg:text-[40px] lg:py-14 '>
                         Legal
                     </Link>
                 </div>
@@ -78,10 +78,10 @@ export default function Root() {
                             opposite Elim church, 740102, Yelwa, Bauchi Nigeria
                         </p>
                     </div>
-                    <div className='px-5 border  flex-1'>
+                    <div className='px-5 border flex-1'>
                         <div className='text-xl font-medium my-6'>External</div>
 
-                        <div className='flex m-auto  mb-6 flex-row justify-between w-[230px]'>
+                        <div className='flex m-auto  flex-row justify-between w-[230px]'>
                             <TbWorldWww className='text-black text-2xl cursor-pointer' />
 
                             <BsInstagram className='text-black text-2xl cursor-pointer' />
@@ -98,8 +98,8 @@ export default function Root() {
                 </div>
 
                 <div
-                    className={`min-h-screen bg-black w-[100%] absolute top-[15px] rounded-t-[2.7rem] overflow-x-hidden text-slate-200 lg:px-[2.5rem] lg:top-[5px] ${page.dropdown
-                        ? 'transition-transform translate-y-[100vh] lg:translate-y-[100vh]  ease-in-out duration-500'
+                    className={`min-h-screen bg-black w-[100%] absolute top-0  lg:rounded-t-[2.7rem] overflow-x-hidden text-slate-200 lg:px-[2.5rem] lg:top-[5px] ${page.dropdown
+                        ? 'transition-transform translate-y-[100vh] lg:translate-y-[100vh]  ease-in-out rounded-t-[2.7rem] duration-500 top-[18rem] '
                         : 'transition-transform translate-y-0 display-hidden ease-in-out duration-300'
                         }`}
                 >
@@ -133,7 +133,7 @@ export default function Root() {
 
                     <footer className='w-[100%] m-auto h-[100%] mt-[10vh] flex flex-col '>
                         <div className='w-[100%]  flex justify-center m-auto flex-col lg:flex-row lg:justify-between'>
-                            <div className='w-[100%] px-[2.5rem] m-auto flex  mt-0 justify-around  lg:flex-row lg:w-[40%] lg:justify-between'>
+                            <div className='w-[100%] px-[2.5rem] gap-2 m-auto flex  mt-0 justify-around  lg:flex-row lg:w-[40%] lg:justify-between'>
                                 <div className='h-[100%] flex flex-col'>
                                     <h4 className='text-lg font-bold my-1'>Internal</h4>
                                     <Link to='/' className='py-2'>
@@ -153,7 +153,7 @@ export default function Root() {
                                     </Link>
                                 </div>
 
-                                <div className='h-full flex flex-col mx-4'>
+                                <div className='h-full flex flex-col'>
                                     <h4 className='text-lg font-bold my-1'>Documentation</h4>
                                     <Link to='/' className='py-2'>
                                         About us
@@ -166,7 +166,7 @@ export default function Root() {
                                     </Link>
                                 </div>
 
-                                <div className='h-full flex flex-col mx-2'>
+                                <div className='h-full flex flex-col  lg:mx-2'>
                                     <h4 className='text-lg font-bold my-1'>External</h4>
                                     <Link to='zutayah.com' className='py-2'>
                                         Zutayah
@@ -219,7 +219,7 @@ export default function Root() {
                             </form>
                         </div>
 
-                        <div className='grow-[.3] px-[1.5rem] py-16 w-[100%] border-t border-slate-950 flex flex-row justify-between items-center'>
+                        <div className='grow-[.3] px-[1.5rem] py-16 w-[100%] gap-5 border-t border-slate-950 flex flex-row justify-between items-center'>
                             <div>
                                 <p
                                     className='text-xl tracking-widest font-bold cursor-pointer'
