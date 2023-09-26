16a750cce4fb369963999541a0bb68d2d35d6f60
beekay19 <bknelson19@gmail.com>
2023-09-26 11:56:17 +0100

    added img carousel back to interface

--- src/components/interface/layout/Carousel.jsx
+++ src/components/interface/layout/Carousel.jsx
@@ -1,55 +1,78 @@
-import React, { useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useState,useRef } from "react";
 import CarouselItems from "./assets/carousel/CarouselItems";
-
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import img from "../layout/assets/annie-spratt-ixtJA53Z0CU-unsplash.jpg";
 import img1 from "../layout/assets/victoria-nazaruk-2cpW5zq93yY-unsplash.jpg";
 import img2 from "../layout/assets/ranurte-kSdi_gqbGGs-unsplash.jpg";
+import img3 from "../layout/assets/karsten-winegeart-Ss0A5IX6-XE-unsplash.jpg";
+import img4 from "../layout/assets/musa-ortac-A6uObQ0JjGM-unsplash.jpg";
 
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+
+import { FaChevronLeft, FaChevronRight,FaExpand,FaDownload,FaFileDownload } from "react-icons/fa";
+
 
 export const Carousel = () => {
-  const list = [img, img1, img2];
+  const list = [img, img1, img2,img3,img4];
   const [activeIndex, setActiveIndex] = useState(0);
-
+  const [enableFullscreen,setEnableFullScreen] = useState(false)
+  const handle = useFullScreenHandle();
+  const userRef = useRef()
   const updateIndex = (index)=>{
     if(index < 0){
        index = 0
-    }else if(index >= 3){
+    }else if(index >= list.length){
         index = 0
     }
     setActiveIndex(index)
-
+  }
+  const handleClick = ()=>{
+    
+  }
+  const onClick = ()=>{
+    if(enableFullscreen){
+      handle.exit()
+    }else{
+      handle.enter()
+    }
+    setEnableFullScreen((prevState) => !prevState)    
   }
 
   return (
-    <div className="carousel relative h-[600px] w-[80%] mx-auto">
+    
+    <FullScreen handle={handle}>
+    <div className={`carousel relative h-[600px] w-[90%]  mx-auto ${enableFullscreen && 'h-full'}`}ref={userRef}>
+      
       <button
-        className="absolute top-1/2 -translate-y-1/2 z-10 -left-12"
+        className={`absolute top-1/2 -translate-y-1/2 z-10  ${enableFullscreen? 'left-0' : '-left-12'}`}
         onClick={() => updateIndex(activeIndex - 1)}
       >
-        <FaChevronLeft className="w-8 h-8" />
+        <FaChevronLeft  className={`${enableFullscreen ? 'text-white':'text-black'} w-8 h-8`} />
       </button>
       <div className="carousel__track-container h-full relative">
-    <div className="h-full w-full">
-    <ul
-          className="carousel__track h-full w-full flex overflow-hidden "
-        >
-          {list.map((item, index) => (
-            <CarouselItems
-              key={index}
-              active={activeIndex}
-              item={item}
-              width={"100%"}
-            />
-          ))}
-        </ul>
-    </div>
+    <ul className="h-full w-full inline-flex overflow-hidden" >
+      {list.map((item,index) => {
+      return  <CarouselItems key={index}  item={item} active={activeIndex}/>
+      })}
+    </ul>
+
       </div>
       <button
-        className="absolute top-1/2 -translate-y-1/2 -right-12"
+        className={`absolute top-1/2 -translate-y-1/2 ${enableFullscreen? 'right-0' : '-right-12'}`}
         onClick={() => updateIndex(activeIndex + 1)} >
-        <FaChevronRight className="w-8 h-8" />
+        <FaChevronRight className={`${enableFullscreen ? 'text-white':'text-black'} w-8 h-8`} />
       </button>
+      
+      <button type="button" className="absolute right-1 z-50 bottom-14">
+      <FaExpand size='30px' onClick={onClick} color="#eee" />
+      </button>
+      <button type="button" className="absolute right-1 bottom-4">
+      <FaDownload size='30px'/>
+      </button>
+ 
     </div>
+
+    </FullScreen>
   );
 };
--- src/components/root/root.jsx
+++ src/components/root/root.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import { Outlet } from 'react-router';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
