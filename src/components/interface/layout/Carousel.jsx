78101d81974217ff8aec762a4b53290625bb00eb
beekay19 <bknelson19@gmail.com>
2023-10-04 19:06:49 +0100

    added

--- src/components/interface/layout/Carousel.jsx
+++ src/components/interface/layout/Carousel.jsx
@@ -140,7 +140,7 @@ export const Carousel = ({ nav }) => {
         onMouseMove={() => {
           debouncedFunctionTrail(), debouncedFunctionLead();
         }}
-       
+        
         onTouchStart={() => {
           debouncedFunctionTrail(), debouncedFunctionLead();
         }}
