1be9507b1f7546efa34823c3cb4622a326137292
beekay19 <bknelson19@gmail.com>
2023-10-09 15:49:33 +0100

    fetched lastest repo

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,25 +1,26 @@
+/* eslint-disable no-undef */
 export const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
 
-export const SERVER_URL = 'http://localhost:4000';
+export const SERVER_URL = "http://localhost:4000";
 // https://pptlink-node-backend.onrender.com/
 // export const SERVER_URL =
 //   process.env.NODE_ENV === 'development' ? 'http://10.42.0.1:4000' : '';
 
-export const HOME = '/';
+export const HOME = "/";
 
-export const INSTITUTIONS = '/institutions';
+export const INSTITUTIONS = "/institutions";
 
-export const INSTITUTIONS_ID = '/institutions/:id';
+export const INSTITUTIONS_ID = "/institutions/:id";
 
-export const ABOUT = '/about';
+export const ABOUT = "/about";
 
-export const DASHBOARD = '/dashboard';
+export const DASHBOARD = "/dashboard";
 
-export const LEGAL = '/legal';
+export const LEGAL = "/legal";
 
-export const UPLOAD = '/upload';
+export const UPLOAD = "/upload";
 
-export const SIGNUP = '/signup';
+export const SIGNUP = "/signup";
 
-export const LOGIN = '/login';
+export const LOGIN = "/login";
