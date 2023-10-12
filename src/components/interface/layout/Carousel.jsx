82091b6d12a0f5881c6a8810d712468ffbe30482
Omizegba <flamanyoman@gmail.com>
2023-10-12 13:31:42 +0100

    Go live button fixed

--- src/components/interface/layout/Carousel.jsx
+++ src/components/interface/layout/Carousel.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable no-irregular-whitespace */
 /* eslint-disable no-unused-vars */
 
-
 import { useState, useRef, useEffect } from 'react';
 import debounce from 'lodash.debounce';
 import SwiperMySlide from './assets/carousel/Swiper';
@@ -14,15 +13,18 @@ import { ToastContainer } from 'react-toastify';
 import CopyAllRounded from '@mui/icons-material/CopyAllOutlined';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import { Button } from '@mui/material';
-import { LoadingAssetSmall, LoadingAssetSmall2 } from '../../../assets/assets';
-
+import { LoadingAssetSmall2 } from '../../../assets/assets';
 
 let stopFunction = false;
 let navBar = false;
 
-
-export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) => {
-
+export const Carousel = ({
+  nav,
+  presentation,
+  makeLive,
+  socket,
+  livePending,
+}) => {
   const [active, setActive] = useState(false);
   const [enableFullscreen, setEnableFullScreen] = useState(false);
   const userRef = useRef();
@@ -86,7 +88,7 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
   const debouncedFunctionLead = debounce(
     () => {
       setActive(true);
-      console.log("wait");
+      console.log('wait');
     },
     200,
     { leading: true, trailing: false }
@@ -108,15 +110,15 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
       }));
     }
 
-    screen.orientation.addEventListener("change", (event) => {
-      if (event.target.type.includes("landscape") && window.innerWidth < 900) {
+    screen.orientation.addEventListener('change', (event) => {
+      if (event.target.type.includes('landscape') && window.innerWidth < 900) {
         setSpecialMedia((prev) => ({
           ...prev,
           animation1: false,
           animation2: true,
         }));
       } else if (
-        event.target.type.includes("portrait") &&
+        event.target.type.includes('portrait') &&
         window.innerWidth < 900
       ) {
         setSpecialMedia((prev) => ({
@@ -132,7 +134,7 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
     <>
       <div
         className={`carousel relative lg:h-[650px] w-[100%] overflow-y-auto  mx-auto select-none ${
-          enableFullscreen && "h-full w-full rotate-90"
+          enableFullscreen && 'h-full w-full rotate-90'
         }`}
         ref={userRef}
         onMouseMove={() => {
@@ -144,42 +146,39 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
       >
         {active && (
           <p
-            className="max-w-full bg-[white]/10 backdrop-blur-md text-[white]/50 absolute z-[10] left-4 top-6 lg:hidden py-3 px-2 rounded-md md:max-w-sm flex justify-between "
+            className='max-w-full bg-[white]/10 backdrop-blur-md text-[white]/50 absolute z-[10] left-4 top-6 lg:hidden py-3 px-2 rounded-md md:max-w-sm flex justify-between '
             onClick={() => {
               navigator.clipboard &&
                 navigator.clipboard.writeText(window.location.href);
-              toast.success("Link Copied successfully");
+              toast.success('Link Copied successfully');
             }}
           >
             <span>{window.location.href}</span> <CopyAllRounded />
           </p>
         )}
 
-        <div className="carousel__track-container h-full relative">
-          <ul className="h-full w-full flex  ">
+        <div className='carousel__track-container h-full relative'>
+          <ul className='h-full w-full flex  '>
             <SwiperMySlide
               list={presentation.imageSlides}
               active={active}
               socket={socket}
               presentation={presentation}
             />
-
-
-
           </ul>
         </div>
         <div
           className={`absolute lg:hidden z-20 top-6 right-6  ${
-            active ? "block" : "hidden"
+            active ? 'block' : 'hidden'
           }`}
         >
           {presentation.User === 'HOST' && (
             <Button
               title={presentation.live ? 'End live' : 'Go live'}
               onClick={makeLive}
-              className={` m-w-32 !text-slate-200 !rounded-xl space-x-2  ${
+              className={`w-[140px] h-[40px] !text-slate-200 !rounded-xl space-x-2 ${
                 presentation.live ? '!bg-rose-500/50' : ' !bg-green-500/50'
-              }  `}
+              }`}
             >
               {livePending ? (
                 <LoadingAssetSmall2 />
@@ -197,8 +196,8 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
 
         <nav
           className={`h-16 w-16 rounded-full bottom-12 right-12  z-30 fixed transition-all duration-500 ${
-            navbar ? "" : "active"
-          } ${active ? "block" : "hidden"}`}
+            navbar ? '' : 'active'
+          } ${active ? 'block' : 'hidden'}`}
           onMouseEnter={() => {
             stopFunction = true;
           }}
@@ -212,17 +211,17 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
             className={`w-full h-full flex items-center justify-center select-none`}
           >
             <button
-              title="Toggle fullscreen"
-              aria-label="Toggle fullscreen"
-              type="button"
+              title='Toggle fullscreen'
+              aria-label='Toggle fullscreen'
+              type='button'
               className={`absolute -left-14 z-50 rounded-full bg-black p-2 bottom-2
-            ${active ? "block" : "hidden"}
+            ${active ? 'block' : 'hidden'}
           `}
             >
               <FaExpand
-                size="30px"
+                size='30px'
                 onClick={() => toggleFullScreen()}
-                className="text-slate-200"
+                className='text-slate-200'
               />
             </button>
 
@@ -234,7 +233,7 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
                 console.log(stopFunction);
               }}
               className={`text-slate-200 text-2xl rounded-full border active:scale-75 duration-200 border-white bg-black flex items-center z-20 justify-center w-full h-full active:bg-slate-200 transition-all select-none ${
-                navBar ? "rotate-180" : "rotate-0"
+                navBar ? 'rotate-180' : 'rotate-0'
               }`}
             >
               <FaChevronUp />
@@ -243,24 +242,24 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
               <li
                 key={i}
                 className={`absolute w-[80%] rounded-full p-2 h-[80%] bg-black z-10 text-slate-200 border border-white transition-all  duration-500 ${
-                  navbar && "duration-300"
+                  navbar && 'duration-300'
                 }`}
                 style={{
                   transform: navbar
                     ? `translatey(-${(i + 1.1) * 100 + (i + 1) * 8}%)`
-                    : "translateY(0)",
+                    : 'translateY(0)',
                   transitionDelay: `${(i + 1) / 10}s`,
                   zIndex: navItems.length - (i + 1),
                 }}
               >
                 <a
                   href={link}
-                  className="w-full relative h-full flex items-center  justify-center flex-row-reverse"
+                  className='w-full relative h-full flex items-center  justify-center flex-row-reverse'
                 >
-                  <span className="">{icon}</span>
+                  <span className=''>{icon}</span>
                   <span
                     className={`text-white absolute right-[calc(100%+1rem)] rounded-md p-2 shadow-md transition-all border-white border ${
-                      navBar ? "opacity-100" : "opacity-0"
+                      navBar ? 'opacity-100' : 'opacity-0'
                     } duration-500 font-bold bg-black`}
                   >
                     {name}
@@ -274,32 +273,32 @@ export const Carousel = ({ nav, presentation, makeLive, socket,livePending }) =>
       </div>
 
       {specialMedia.toggled && (
-        <div className="w-full h-screen fixed top-0 left-0 bottom-0 bg-black z-50">
+        <div className='w-full h-screen fixed top-0 left-0 bottom-0 bg-black z-50'>
           {specialMedia.animation1 && (
-            <div className="w-full h-full grid place-content-center">
-              <div className="w-fit h-fit flex flex-col justify-between">
-                <img src={animation1} alt="animation image" />
+            <div className='w-full h-full grid place-content-center'>
+              <div className='w-fit h-fit flex flex-col justify-between'>
+                <img src={animation1} alt='animation image' />
 
-                <p className="text-slate-200">Rotate to landscape mode</p>
+                <p className='text-slate-200'>Rotate to landscape mode</p>
               </div>
             </div>
           )}
 
           {!specialMedia.animation1 && specialMedia.animation2 && (
-            <div className="w-full h-full grid place-content-center">
-              <div className="w-fit h-fit flex flex-col justify-between">
+            <div className='w-full h-full grid place-content-center'>
+              <div className='w-fit h-fit flex flex-col justify-between'>
                 <FaExpand
                   onClick={removeSpecialMedia}
-                  className="absolute text-slate-200 text-[70px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
+                  className='absolute text-slate-200 text-[70px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'
                 />
 
                 <img
                   src={animation2}
-                  alt="animation image"
-                  className="mt-[3rem] ml-[1.5rem] z-10 pointer-events-none"
+                  alt='animation image'
+                  className='mt-[3rem] ml-[1.5rem] z-10 pointer-events-none'
                 />
 
-                <p className="text-slate-200">Click to make full screen</p>
+                <p className='text-slate-200'>Click to make full screen</p>
               </div>
             </div>
           )}
