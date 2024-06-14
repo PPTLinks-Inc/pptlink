289a74b928552b338a5ed490cd28a08e00b0c108
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-14 16:57:55 +0100

    fixing sentry

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
         </Routes>
       </AnimatePresence>
 
-      <ToastContainer stacked />
+      {/* <ToastContainer stacked /> */}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import * as Sentry from "@sentry/react";
 import "./index.css";
 import App from "./App";
 import UserContextProvider from "./contexts/userContext";
 import { BrowserRouter as Router } from "react-router-dom";
+import * as Sentry from "@sentry/react";
 
 const queryClient = new QueryClient();
 
+
 Sentry.init({
   dsn: "https://805f4f57fe05b40dad095ee1cabc09d4@o4507420306898944.ingest.de.sentry.io/4507420319350864",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", "https://pptlinks.com", "https://pptlinks-backend.onrender.com"],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+  integrations: []});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
