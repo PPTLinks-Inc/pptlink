a7be53ef16cfc88f0e0e7d53da91c3759c9bb7bb
Raymondamem <raymondamem525@gmail.com>
2024-10-11 14:59:51 +0100

    working on it

--- src/components/upload/presentationStages/uploadStage.jsx
+++ src/components/upload/presentationStages/uploadStage.jsx
@@ -4,9 +4,16 @@ import img_featherTwo from "/Group92.png";
 
 export default function UploadStage({ currentView, uploadValues, setUploadValues, handleInputChange }) {
     const [addCategory, setAddCategory] = useState({ isNewCategory: false, newCategory: "" });
+    const [addCategoryError, setAddCategoryError] = useState(false);
     // make axios request to update categories here
     function updateCategories() {
-        setAddCategory(prev => ({ ...prev, isNewCategory: false }));
+        if (addCategory.newCategory === "") {
+            setAddCategoryError(true);
+            return;
+        } else {
+            setAddCategoryError(false);
+        }
+        setAddCategory((prev) => ({ ...prev, isNewCategory: false }));
         console.log("Added categories", addCategory);
     }
 
@@ -158,13 +165,12 @@ export default function UploadStage({ currentView, uploadValues, setUploadValues
             </div>
             {/* Add Category */}
             <div className="flex justify-between w-[90%] m-auto mt-6">
-                <div className="w-[48%] maxScreenMobile:w-full mr-auto flex flex-col justify-center _items-center h-fit mt-6 text-lg text-black">
+                <div className="w-[48%] maxScreenMobile:w-full mr-auto flex flex-col justify-center items-center h-fit mt-6 text-lg text-black">
                     <div className="w-full relative">
                         <label htmlFor="publicSelector" className="block mb-2">
                             <sup className="w-full text-xl font-bold">*</sup>Category
                         </label>
-                        <div
-                            className={`bg-white h-fit justify-between items-center overflow-hidden flex w-full indent-4 focus:outline focus:outline-[1px] shadow-md rounded-md`}
+                        <div className={`bg-white h-fit justify-between items-center overflow-hidden flex w-full indent-4 focus:outline focus:outline-[1px] shadow-md rounded-md ${addCategoryError && "border-2 border-[red]"}`}
                         >
                             <select
                                 name="category"
@@ -187,7 +193,10 @@ export default function UploadStage({ currentView, uploadValues, setUploadValues
 
                             <button
                                 type="button"
-                                onClick={() => setAddCategory(prev => ({ ...prev, newCategory: "", isNewCategory: !prev.isNewCategory }))}
+                                onClick={() => {
+                                    setAddCategoryError(false);
+                                    setAddCategory(prev => ({ ...prev, newCategory: "", isNewCategory: !prev.isNewCategory }));
+                                }}
                                 className="w-[40%] maxScreenMobile:text-[0.8rem] 
                       flex gap-1 justify-evenly items-center h-full p-2 
                       bg-black border-black rounded-tr-md rounded-br-md maxScreenMobile:w-fit maxScreenMobile:p-4"
@@ -222,14 +231,7 @@ export default function UploadStage({ currentView, uploadValues, setUploadValues
                             </button>
                         </div>
                     </div>
-
-                    {false ? (
-                        <p className="text-[red]">{""}</p>
-                    ) : (
-                        false && (
-                            <p className="text-[red]">{""}</p>
-                        )
-                    )}
+                    {addCategoryError && (<p className="text-[red] w-full">{"Category already exists or is invalid"}</p>)}
                 </div>
             </div>
         </div>
