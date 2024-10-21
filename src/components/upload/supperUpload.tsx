ff632fd3af0a5ec5579dac3f881693eb291cdedd
Yohanna Abana <yohanaabana02@gmail.com>
2024-10-21 16:13:53 +0100

    can now update and manually check file processing status

--- src/components/upload/supperUpload.tsx
+++ src/components/upload/supperUpload.tsx
@@ -8,10 +8,16 @@ import UploadStage from "./presentationStages/uploadStage";
 import InformationStage from "./presentationStages/informationStage";
 import PreviewStage from "./presentationStages/previewStage";
 import { useUploadStore } from "@/store/uploadStore";
+import { useSearchParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { LoadingAssetBig2 } from "@/assets/assets";
 
 export default function SupperUpload() {
   const scrollableRef = useRef<HTMLDivElement>(null);
   const currentView = useUploadStore((state) => state.currentView);
+  const resetUploadStore = useUploadStore((state) => state.resetStore);
+  const [searchParams] = useSearchParams();
 
   // scroll page to the top when currentView changes
   useEffect(() => {
@@ -20,25 +26,67 @@ export default function SupperUpload() {
     }
   }, [currentView]);
 
+  useEffect(function() {
+    return function() {
+      resetUploadStore();
+    };
+  }, []);
+
+  const editPresentationQuery = useQuery({
+    queryKey: ["editPresentation", searchParams.get("edit")],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `/api/v1/ppt/presentations/${searchParams.get("edit")}`
+      );
+
+      useUploadStore.setState({
+        title: data.presentation.name,
+        description: data.presentation.description,
+        privacy: data.presentation.linkType,
+        downloadable: data.presentation.downloadable,
+        selectedCategory: data.presentation.category,
+        presentersName: data.presentation.presenterName,
+        bio: data.presentation.bio,
+        socialLinks: data.presentation.social_media,
+        date: data.presentation.date,
+        startTime: data.presentation.startTime,
+        endTime: data.presentation.endTime,
+        pdfUrl: data.presentation.thumbnail
+      });
+
+      return data;
+    },
+    enabled: searchParams.has("edit")
+  });
+
   return (
     <section
       ref={scrollableRef}
       className="upload_svg_cover h-fit relative bg-[#FFFFF0]"
     >
       <div className="bottom_cover_ pt-10 pb-16 w-[90%] m-auto bg-transparent h-fit z-50 maxScreenMobile:w-full">
-        {/* progress indicator */}
-        <ProgressIndicator />
-        <div className="w-full min-h-screen bg-[#FFFFF0] shadow-xl relative py-20 maxScreenMobile:pt-0 md:rounded-md">
-          <FormLabelIndicator />
-          {/* first stage elements 👀👀 */}
-          <UploadStage />
-          {/* Second stage show els 👀👀 */}
-          <InformationStage />
-          {/* Third stage show els 👀👀 */}
-          <PreviewStage />
-        </div>
-        {/* form stage tracker */}
-        <FormStageMover />
+        {editPresentationQuery.isLoading ? (
+          <div className="flex flex-col justify-center items-center min-h-screen">
+            <LoadingAssetBig2 />
+            <p className="text-center">Fetching presentation data</p>
+          </div>
+        ) : (
+          <>
+            {/* progress indicator */}
+            <ProgressIndicator />
+            <div className="w-full min-h-screen bg-[#FFFFF0] shadow-xl relative py-20 maxScreenMobile:pt-0 md:rounded-md">
+              <FormLabelIndicator />
+              {/* first stage elements 👀👀 */}
+              <UploadStage />
+              {/* Second stage show els 👀👀 */}
+              <InformationStage />
+              {/* Third stage show els 👀👀 */}
+              <PreviewStage />
+            </div>
+            {/* form stage tracker */}
+            <FormStageMover />
+          </>
+        )}
       </div>
     </section>
   );
