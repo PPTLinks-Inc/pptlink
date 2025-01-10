605ab25caaa0b39a00bfd8a818b1ae4fd1fa3b67
Yohanna Philip  Abana <57155932+yohanna02@users.noreply.github.com>
2025-01-10 14:19:54 +0100

    Update routes.ts

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -6,10 +6,10 @@ export enum MIC_STATE {
   MIC_MUTED = "MIC_MUTED"
 };
 
-export const SERVER_URL = "http://localhost:4000";
+// export const SERVER_URL = "http://localhost:4000";
 // export const SERVER_URL = "https://verbose-telegram-q44qrpxqrvpcgjr-4000.app.github.dev";
 
-// export const SERVER_URL = "https://api.pptlinks.com";
+export const SERVER_URL = "https://api.pptlinks.com";
 
 export const AGORA_APP_ID = "33672aae3d8d48f4b83292008db3986e";
 
