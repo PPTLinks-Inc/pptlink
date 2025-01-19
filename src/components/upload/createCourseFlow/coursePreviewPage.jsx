fdf24e7ae1e3c28c65fa341280a46d33910157bc
Raymondamem <raymondamem525@gmail.com>
2025-01-19 07:41:47 +0100

    made courses card

--- src/components/upload/createCourseFlow/coursePreviewPage.jsx
+++ src/components/upload/createCourseFlow/coursePreviewPage.jsx
@@ -1,8 +1,10 @@
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import AccordionWrapper from "../../accordion/accordion";
 import LogoBlack from "../../../images/Logo-Black.png";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import CourseCard from "../../list/courseCard";
+import InstructorCard from "../../list/instructorCard";
 
 export default function CoursePreviewPage() {
     return (<>
@@ -41,7 +43,7 @@ export default function CoursePreviewPage() {
             />
             <meta property='twitter:image' content={LogoBlack} />
         </Helmet>
-        <section className="bg-primaryTwo">
+        <div className="bg-primaryTwo">
             <div className="container h-fit py-10 flex flex-col justify-between items-start">
                 <h1 className="text-3xl font-[400] uppercase mb-5">UI/UX Strategy Essentials</h1>
                 <p className="uppercase text-[#FFA500]">Paid Program</p>
@@ -66,7 +68,7 @@ export default function CoursePreviewPage() {
                     <span className="text-[#FFA500] font-bold text-xl">$45,330</span>
                 </div>
             </div>
-        </section>
+        </div>
         <div className="border-t-2 border-b-2 bg-primaryTwo border-t-white border-b-white">
             <div className="container py-3 flex justify-between items-center gap-2 maxScreenMobile:flex-col maxScreenMobile:items-start">
                 <p><span>#</span>3 Months</p>
@@ -74,7 +76,7 @@ export default function CoursePreviewPage() {
                 <p><span>#</span>Last Uploaded August 17 2024</p>
             </div>
         </div>
-        <section className="bg-primaryTwo min-h-[50vh]">
+        <section className="bg-primaryTwo min-h-[30vh]">
             <h2 className="container text-xl font-bold text-white capitalize pt-8 pb-4">Courses in this Program</h2>
             {/* start */}
             {Array.from({ length: 4 }, (_, i) => i + 1).map(idx => (
@@ -90,15 +92,8 @@ export default function CoursePreviewPage() {
                     >
                         <div className="courses my-4 grid grid-cols-4 auto-rows-max grid-flow-row gap-4 maxScreen:grid-cols-3 maxScreenMobile:grid-cols-2 maxSmallMobile:grid-cols-1">
                             {/* start */}
-                            {Array.from({ length: (1 + idx) }, (_, i) => i + 1).map(idx => (
-                                <div key={idx.toString()} className="border-2 border-[#FFFFF0] rounded-md p-3">
-                                    <img src="/team/imoh.jpg" alt="imoh" className="block mb-3 w-12 aspect-square border-0 rounded-md object-cover" />
-                                    <div className="flex flex-col justify-between items-start gap-2">
-                                        <h3 className="font-semibold text-base">Ideation and Validation</h3>
-                                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, dolor qui beatae corrupti at numquam delectus iusto quia in, dignissimos alias. Minus repudiandae at incidunt hic nihil unde nulla odit?</p>
-                                        <NavLink to="#" target="_blank" className="block underline">Learn More...</NavLink>
-                                    </div>
-                                </div>
+                            {Array.from({ length: (1 + idx) }, (_, i) => i + 1).map(idx2 => (
+                                <CourseCard key={idx2.toString()} img={"/team/yoh.jpg"} type={(1 + idx2)} />
                             ))}
                             {/* end */}
                         </div>
@@ -107,37 +102,14 @@ export default function CoursePreviewPage() {
             ))}
             {/* end */}
         </section>
-        <section className="bg-primaryTwo w-full pb-4">
+        <section className="bg-primaryTwo w-full pb-16">
             <h3 className="container text-2xl text-center font-semibold pb-6 uppercase">Your Course Instructor</h3>
             <div className="instructors container my-4 grid grid-cols-4 auto-rows-max grid-flow-row gap-4 maxScreen:grid-cols-3 maxScreenMobile:grid-cols-2 maxSmallMobile:grid-cols-1">
                 {/* start */}
                 {Array.from({ length: 4 }, (_, i) => i + 1).map(idx => (
-                    <div key={idx.toString()} className="border-2 border-[#FFFFF0] rounded-md p-3">
-                        <img src="/team/imoh.jpg" alt="imoh" className="block mb-3 w-12 aspect-square border-0 rounded-md object-cover" />
-                        <div className="flex flex-col justify-between items-start gap-2">
-                            <h3 className="font-semibold text-base">Raahi H. Aljareem</h3>
-                            <p className="text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, dolor qui beatae corrupti at numquam delectus iusto quia in, dignissimos alias. a odit?</p>
-                            <NavLink to="#" target="_blank" className="block underline">Learn More...</NavLink>
-                        </div>
-                    </div>
+                    <InstructorCard key={idx.toString()} img={"/team/imoh.jpg"} />
                 ))}
                 {/* end */}
-            </div>
-        </section>
-        <section className="bg-primaryTwo w-full pt-8 pb-16">
-            <h3 className="container text-2xl text-center font-semibold pb-2 uppercase">PPTLINKS main selling points</h3>
-            <p className="container text-sm text-center pb-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum quas, ullam odit consectetur suscipit corrupti nostrum tempora?</p>
-            <div className="instructors container my-4 grid grid-cols-4 auto-rows-max grid-flow-row gap-4 maxScreen:grid-cols-3 maxScreenMobile:grid-cols-2 maxSmallMobile:grid-cols-1">
-                {Array.from({ length: 4 }, (_, i) => i + 1).map(idx => (
-                    <div key={idx.toString()} className="border-2 border-[#FFFFF0] rounded-md p-3">
-                        <img src="/team/imoh.jpg" alt="imoh" className="block mb-3 w-12 aspect-square border-0 rounded-md object-cover" />
-                        <div className="flex flex-col justify-between items-start gap-2">
-                            <h3 className="font-semibold text-base">Raahi H. Aljareem</h3>
-                            <p className="text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, dolor qui beatae corrupti at numquam delectus iusto quia in, dignissimos alias. a odit?</p>
-                            <NavLink to="#" target="_blank" className="block underline">Learn More...</NavLink>
-                        </div>
-                    </div>
-                ))}
             </div>
         </section>
     </>)
