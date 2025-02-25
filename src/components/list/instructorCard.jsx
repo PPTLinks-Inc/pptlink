a03e1382185598c590e14f5f6df3801c8eb25d1a
Yohanna Abana <yohanaabana02@gmail.com>
2025-02-25 01:04:58 +0100

    Implement video player functionality and enhance course card interactions

--- src/components/list/instructorCard.jsx
+++ src/components/list/instructorCard.jsx
@@ -8,7 +8,7 @@ export default function InstructorCard({ data }) {
             <img src={data.instructor.photo} alt={data.instructor.user.username} className="block mb-3 w-12 aspect-square border-0 rounded-full object-cover" />
             <div className="flex flex-col justify-between items-start gap-2">
                 <h3 className="font-semibold text-base">{data.instructor.user.username}</h3>
-                <p className="text-sm line-clamp-4">{data.instructor.bio}</p>
+                <p className="text-sm line-clamp-4 whitespace-pre-wrap w-full">{data.instructor.bio}</p>
                 <NavLink to={"#"} target="_blank" className="block underline">Learn More...</NavLink>
             </div>
         </div>
