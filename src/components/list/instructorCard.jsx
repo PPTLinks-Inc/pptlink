0bcf0a38e74ccd9d40ba12a3e107c17fc1866241
Raymondamem <raymondamem525@gmail.com>
2025-03-01 12:21:35 +0100

    fixed instructor card

--- src/components/list/instructorCard.jsx
+++ src/components/list/instructorCard.jsx
@@ -4,11 +4,13 @@ import { NavLink } from "react-router-dom";
 export default function InstructorCard({ data }) {
 
     return (
-        <div className="aspect-square border-2 border-[#FFFFF0] rounded-md p-3">
-            <img src={data.instructor.photo} alt={data.instructor.user.username} className="block mb-3 w-12 aspect-square border-0 rounded-full object-cover" />
+        <div className="w-full h-fit border-[1px] border-[#FFFFF0] rounded-md p-3 flex flex-col justify-between items-start gap-3">
+            <img src={data.instructor.photo} alt={data.instructor.user.username} className="block mb-3 !w-12 !aspect-square border-0 rounded-full object-cover text-sm overflow-hidden" />
             <div className="flex flex-col justify-between items-start gap-2">
-                <h3 className="font-semibold text-base">{data.instructor.user.username}</h3>
-                <p className="text-sm line-clamp-4 whitespace-pre-wrap w-full">{data.instructor.bio}</p>
+                <h3 className="font-semibold text-base line-clamp-2 whitespace-pre-wrap _h-10 _overflow-hidden text-ellipsis">{data.instructor.user.username}</h3>
+                <p className="text-sm line-clamp-4 whitespace-pre-wrap w-full h-20 overflow-x-hidden text-ellipsis">
+                    {data.instructor.bio}
+                    </p>
                 <NavLink to={"#"} target="_blank" className="block underline">Learn More...</NavLink>
             </div>
         </div>
