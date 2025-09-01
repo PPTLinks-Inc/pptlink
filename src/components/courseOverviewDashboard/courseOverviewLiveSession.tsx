2d56601dc1f9907e13fee055397f03111db86ade
Raymondamem <raymondamem525@gmail.com>
2025-09-01 20:50:27 +0100

    done creating layout, working...

---
+++ src/components/courseOverviewDashboard/courseOverviewLiveSession.tsx
@@ -0,0 +1,15 @@
+import CourseOverviewRoot from "./courseOverviewRoot";
+
+export default function CourseOverviewLiveSession() {
+  return (
+    <CourseOverviewRoot>
+      <div className="wrapper w-full h-full grid grid-cols-1 grid-rows-[1fr_1fr] gap-2 bg-white text-black">
+        <h2>Live Session</h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui minima ipsa
+        labore numquam magnam id laudantium repellendus quisquam pariatur
+        exercitationem accusamus nesciunt voluptate, ut aspernatur! Voluptatem
+        deleniti iste illum ex?
+      </div>
+    </CourseOverviewRoot>
+  );
+}
---
+++ src/components/courseOverviewDashboard/subComponents/innerMainHeader.tsx
@@ -0,0 +1,35 @@
+import { Bell } from "lucide-react";
+import { CiSettings } from "react-icons/ci";
+
+export default function InnerMainHeader() {
+  return (
+    <div className="main-header bg-primaryTwo flex flex-col gap-3 p-2">
+      <div className="flex justify-between items-center">
+        <h2 className="font-bold text-2xl">Hi, Raymond Amem Aondoakura</h2>
+        <button className={`text-3xl font-bold $_{text} _text-[#FFFFF0]`}>
+          <CiSettings />
+        </button>
+      </div>
+      <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center gap-4">
+          <span className="block relative">
+            <Bell />{" "}
+            <span className="block absolute -top-[0.09rem] -right-[0.09rem] bg-primarySixTwo rounded-full w-1.5 h-1.5"></span>
+          </span>
+          <p className="text-white text-sm">You have 2 new notifications. </p>
+        </div>
+        <div className="flex justify-between items-center gap-4">
+          <button className="bg-white text-primaryTwo text-xs px-3 py-2 rounded-md">
+            Upload Slides
+          </button>
+          <button className="bg-white text-primaryTwo text-xs px-3 py-2 rounded-md">
+            Schedule Live Session
+          </button>
+          <button className="bg-white text-primaryTwo text-xs px-3 py-2 rounded-md">
+            Create New Course
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
