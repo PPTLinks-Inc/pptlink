d983e9feee977c9e457d0767542755fab587651f
Raymondamem <raymondamem525@gmail.com>
2025-09-06 09:34:03 +0100

    working

--- src/components/courseOverviewDashboard/courseOverviewRoot.tsx
+++ src/components/courseOverviewDashboard/courseOverviewRoot.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import CourseOverviewHeader from "./subComponents/header";
 import AsideForDarshboardOverview from "./subComponents/aside";
 import InnerMainHeader from "./subComponents/innerMainHeader";
@@ -13,17 +14,41 @@ export default function CourseOverviewRoot({
   const [toggleSidebar, setToggleSidebar] = useState(true);
   const handleSideBar = () => setToggleSidebar(!toggleSidebar);
 
+  const sideBarVariant = {
+    initial: {
+      x: 5,
+      opacity: 0
+    },
+    inView: {
+      x: 0,
+      opacity: 100,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.2,
+        when: "beforeChildren"
+      }
+    }
+  };
+
   return (
     <section className="min-h-screen w-full bg-black text-white grid grid-cols-1 grid-rows-[auto_1fr]">
       <CourseOverviewHeader />
-      <div className={`h-full grid ${toggleSidebar ? "grid-cols-[300px_1fr]" : "grid-cols-1"} grid-rows-1`}>
-        <div className={`aside bg-primaryTwo h-full grid grid-cols-1 grid-rows-[auto_1fr_auto] p-2 ${!toggleSidebar && "!hidden"}`}>
+      <div
+        className={`h-full grid ${toggleSidebar ? "grid-cols-[300px_1fr]" : "grid-cols-1"} grid-rows-1`}
+      >
+        <motion.div
+          variants={sideBarVariant}
+          initial="initial"
+          whileInView="inView"
+          viewport={{ once: true }}
+          className={`aside bg-primaryTwo h-full grid grid-cols-1 grid-rows-[auto_1fr_auto] p-2 ${!toggleSidebar && "!hidden"}`}
+        >
           <AsideForDarshboardOverview />
-        </div>
+        </motion.div>
         <main className="relative w-full h-full grid grid-cols-1 grid-rows-[auto_1fr] gap-2 p-2 pt-0">
           <button
             onClick={() => handleSideBar()}
-            className={`absolute ${toggleSidebar ? "-left-4" : "left-0.5"} top-[50%] -translate-y-[50%] bg-black p-1 border-none rounded-lg`}
+            className={`z-10 absolute ${toggleSidebar ? "-left-4" : "left-0.5"} top-[50%] -translate-y-[50%] bg-black p-1 border-none rounded-lg`}
           >
             {toggleSidebar ? <FaCaretLeft /> : <FaCaretRight />}
           </button>
